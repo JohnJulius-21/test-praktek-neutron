ea96c61c66db6704de4d752771314efbcd6f64d6
John Julius <90562403+JohnJulius-21@users.noreply.github.com>
2024-11-15 13:27:35 +0700

    Update index.blade.php

--- resources/views/penjualan/index.blade.php
+++ resources/views/penjualan/index.blade.php
@@ -26,7 +26,7 @@
             style="width:17px;" class="fas fa-fw fa-plus"></i>
         Tambah Penjualan</a>
 
-    <table id="pelanggan" class="display table table-striped mt-2" style="width:100%">
+    <table id="datatable" class="display table table-striped mt-2" style="width:100%">
         <thead>
             <tr>
                 <th>Nama Pelanggan</th>
